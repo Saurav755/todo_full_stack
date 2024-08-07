3a25691967eb81fce45b795dc59797c0881e928d
Saurav Thapa <155595327+Saurav755@users.noreply.github.com>
2024-08-07 19:25:58 +1000

    removed views

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'To Do' });
+router.get("/", function (req, res, next) {
+  // res.render("index", { title: "To Do" });
+  res.status(200).send("to do api");
 });
 
 export default router;
